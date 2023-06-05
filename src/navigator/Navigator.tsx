2768cf54c2b00278d0832cbec66dd60403fd38b7
Jorge Luis Guardo Romero <deliarosaro112@gmail.com>
2023-06-04 23:52:28 -0500

    Navigator: Se incluyeron todas las rutas

--- src/navigator/Navigator.tsx
+++ src/navigator/Navigator.tsx
@@ -2,6 +2,14 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import routes from "../helpers/routes";
 import Home from "../views/Home/Home";
 import Login from "../views/Login/Login";
+import SignUp from "../views/SignUp/SignUp";
+import Contact from "../views/Contact/Contact";
+import CreateOrganization from "../views/CreateOrganization/CreateOrganization";
+import CreateTourney from "../views/CreateTourney/CreateTourney";
+import DashBoard from "../views/DashBoard/DashBoard";
+import Organizer from "../views/Organizer/Organizer";
+import TournamentStaff from "../views/TournamentStaff/TournamentStaff";
+import Tourneys from "../views/Tourneys/Tourneys";
 const Navigator = () => {
     return ( 
         <Routes>
@@ -12,6 +20,38 @@ const Navigator = () => {
             <Route
                 path={routes.login}
                 element={<Login />}
+            />
+            <Route
+                path={routes.signup}
+                element={<SignUp />}
+            />
+            <Route
+                path={routes.contact}
+                element={<Contact />}
+            />
+            <Route
+                path={routes.createOrganizacion}
+                element={<CreateOrganization />}
+            />
+            <Route
+                path={routes.createTourney}
+                element={<CreateTourney />}
+            />
+            <Route
+                path={routes.dashboard}
+                element={<DashBoard />}
+            />
+            <Route
+                path={routes.organizer}
+                element={<Organizer />}
+            />
+            <Route
+                path={routes.tournamentstaff}
+                element={<TournamentStaff />}
+            />
+            <Route
+                path={routes.tourneys}
+                element={<Tourneys />}
             />
         </Routes>
      );
