18ebfea631325abc671d2019317e260cb26bc1d4
douglasd11 <dguerreroe@unicartagena.edu.co>
2023-06-05 05:03:14 -0500

    inscripcion

--- src/views/Inscription/Inscription.tsx
+++ src/views/Inscription/Inscription.tsx
@@ -1,9 +1,14 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useContext, useEffect, useState } from 'react';
 import Header from '../../component/Header/Header';
 import { Outlet } from 'react-router-dom';
 import Footer from '../../component/Footer/Footer';
 import SectionResults from '../SectionResult/SectionResult';
+import Input from '../../component/Input/Input';
 import Table from '../../component/Table/Table';
+
+import InscripcionContext from '../../context/inscripcion/inscripcionContext';
+import AuthContext from '../../context/authentication/authContext';
+
 
 interface InscriptionProps { }
 
@@ -55,7 +60,37 @@ const columns = {
 
 const rows = data.map((item) => ({ row: item }));
 
+
+
 const Inscription: FC<InscriptionProps> = () => {
+
+  const authContext = useContext(AuthContext);
+  const { autenticado , signUp, usuario, usuarioAutenticado } = authContext;
+
+  const inscripcionContext = useContext(InscripcionContext);
+  const { registroInscripcion } = inscripcionContext;
+
+  const [torneu, setTorneu] = useState(0);
+
+  useEffect(()=> {
+    //usuarioAutenticado()
+  },[])
+
+  const [dataS, setData] = useState({
+    fee: 1,
+    code: torneu,
+    code_participant: 4,   //usuario?.id_usuario,
+    status: 1
+  });
+
+  const onChange = (e: any) => {
+    setTorneu(e.target.value);
+  };
+
+  const handlerInscription = (e: any): void => {
+    e.preventDefault();
+    registroInscripcion(dataS)
+  }
 
   return (
     <div className='results-view view'> <Header inicio={true} perfil={true} />
@@ -65,7 +100,15 @@ const Inscription: FC<InscriptionProps> = () => {
             <div >
                 <div className='section-title'>
                     <h2>{"Nombre del torneo"}</h2>
-                    <button>Solicitar Inscripcion</button>
+                    <Input
+                      type="text"
+                      id="cod_tor"
+                      placeholder="Codigo del torneo"
+                      name="codigo"
+                      value={torneu}
+                      onChange={onChange}
+                    />
+                    <button onClick={handlerInscription}>Solicitar Inscripcion</button>
                 </div>
                 <h3>{"Participantes"}</h3>
             </div>
